34932ee12e6c2b19274c14d6ad8c3be18f0d8b0a
-insert name here- <104537575+TheEva66@users.noreply.github.com>
2024-10-04 10:21:53 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This is stuff for my web design course CS107 at arcadia university use it if you want but no promises the code is good or will get good grades
---
+++ assignment1/app.py
@@ -0,0 +1,11 @@
+#runs a specified html file
+from flask import Flask, send_from_directory
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return send_from_directory('.', 'index.html'), 200
+
+if __name__ == '__main__':
+    app.run(port=5000, host='0.0.0.0')
